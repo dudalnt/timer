9434203fdb5e188416af8bf9dc17148011fdae27
aldudakov <dudakov.a.i.98@gmail.com>
2022-06-02 08:51:17 +0300

    added background

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainTimer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainTimer from './components/main-timer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainTimer,
+}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/background.jpg");
+  width:100%;	
+  height:100%;
+  position:fixed;
+  background-size:100% 100%;
 }
 </style>
---
+++ src/components/main-timer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="timerContainer">
+    <div class="countdownTime">
+      0
+    </div>
+    <div class="countdownTime">
+      0
+    </div>
+    <div class="countdownTime">
+      0
+    </div>
+    <div class="countdownTime">
+      0
+    </div>
+  </div>
+</template>
+
+<style>
+.timerContainer{
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  overflow: auto;
+}
+.countdownTime{
+  font-size: 46px;
+  display: inline-block;
+  margin-left: 5rem;
+}
+</style>
